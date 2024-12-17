38edff55b26edf3833052e8a9af7113e64a51e78
​/Thor(sten)?/ Schwesig <89909507+schwesig@users.noreply.github.com>
2024-12-17 05:34:50 -0500

    Update pick_next_host.py

--- pick_next_host.py
+++ pick_next_host.py
@@ -1,17 +1,12 @@
 file_path = "meeting-hosts.md"
 
 # Write back the updated DataFrame to the Markdown file
-def write_md_file(file_path, df):
+def write_md_file(file_path):
     try:
         with open(file_path, "w") as f:
             f.write("# Meeting Hosts\n\n")
-            f.write("| Name       | Hosted Count | Randomized Sort |\n")
-            f.write("|------------|--------------|-----------------|\n")
         print("File successfully updated.")
     except Exception as e:
         print(f"Error writing to the file: {e}")
 
-write_md_file(file_path, "test")
-
-if __name__ == "__main__":
-    pick_next_host()
+write_md_file(file_path)
